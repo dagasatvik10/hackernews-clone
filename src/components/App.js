1634e5da487eb4ac33048dd2f431b3c059b3e7eb
Satvik Daga <satvik.daga@geminisolutions.in>
2019-04-01 15:56:58 +0530

    Added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-// import CreateLink from './CreateLink'
+import { Route, Switch } from 'react-router-dom'
+import CreateLink from './CreateLink'
+import Header from './Header'
 import LinkList from './LinkList'
 
 class App extends Component {
   render() {
-    return <LinkList />
-    // return <CreateLink />
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
+      </div>
+    )
   }
 }
 
